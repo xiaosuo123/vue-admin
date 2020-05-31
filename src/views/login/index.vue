60528fc27c44c44cca69a130f647d226f24d0e01
unknown <1124841716@qq.com>
2020-05-31 20:48:25 +0800

    构建项目

---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- 这里市登录页面 -->
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
