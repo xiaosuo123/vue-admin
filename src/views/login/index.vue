5f81c3e2bd2c38525dec3249fb7c8ca3dd0ba663
unknown <1124841716@qq.com>
2020-06-03 21:53:46 +0800

    登陆页面的静态制作

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,205 @@
 <template>
-  <div>
-    <!-- 这里市登录页面 -->
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+  <div id="login">
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          v-for="item  in menuTab"
+          :key="item.id"
+          :class="{'current': item.current}"
+          @click="toggleMenu(item)"
+        >{{ item.txt }}</li>
+      </ul>
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        class="login-form"
+        size="medium"
+      >
+        <el-form-item prop="username" class="item-form">
+          <label>邮箱</label>
+          <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="password" class="item-form">
+          <label>密码</label>
+          <el-input
+            type="password"
+            v-model="ruleForm.password"
+            autocomplete="off"
+            minlength="6"
+            maxlength="20"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="passwords" class="item-form" v-show="model === 'register'">
+          <label>重复密码</label>
+          <el-input
+            type="password"
+            v-model="ruleForm.passwords"
+            autocomplete="off"
+            minlength="6"
+            maxlength="20"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code" class="item-form">
+          <label>验证码</label>
+          <el-row :gutter="10">
+            <el-col :span="15">
+              <el-input v-model.number="ruleForm.code" minlength="6" maxlength="6"></el-input>
+            </el-col>
+            <el-col :span="9">
+              <el-button type="success" class="block">获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="danger" @click="submitForm('ruleForm')" class="login-btn block">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import { stripscript } from "../../utils/validate.js";
+export default {
+  name: "login",
+  data() {
+    var validateUsername = (rule, value, callback) => {
+      let reg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/; //邮箱的正则表达式
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else if (!reg.test(value)) {
+        callback(new Error("用户名格式有错误"));
+      } else {
+        callback();
+      }
+    };
+    var validatePass1 = (rule, value, callback) => {
+      this.ruleForm.password = stripscript(value);
+      value = this.ruleForm.password;
+      let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else if (!reg.test(value)) {
+        callback(new Error("密码为6-20位的数字+字母!"));
+      } else {
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      this.ruleForm.passwords = stripscript(value);
+      value = this.ruleForm.passwords;
+      let reg = /^(?!\D+$)(?![^a-zA-Z]+$)\S{6,20}$/;
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value != this.ruleForm.password) {
+        callback(new Error("重复密码不正确"));
+      } else {
+        callback();
+      }
+    };
+    var checkCode = (rule, value, callback) => {
+      this.ruleForm.code = stripscript(value);
+      value = this.ruleForm.code;
+      let reg = /^[a-z0-9]{6}$/;
+      if (!value) {
+        return callback(new Error("请输入验证码"));
+      } else if (!reg.test(value)) {
+        callback(new Error("验证码格式有误"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      menuTab: [
+        { txt: "登录", current: true,type: 'login' },
+        { txt: "注册", current: false, type: 'register' }
+      ],
+      model: 'register',
+      ruleForm: {
+        username: "",
+        password: "",
+        passwords: "",
+        code: ""
+      },
+      rules: {
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [{ validator: validatePass1, trigger: "blur" }],
+        passwords: [{ validator: validatePass2, trigger: "blur" }],
+        code: [{ validator: checkCode, trigger: "blur" }]
+      }
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach(element => {
+        element.current = false;
+      });
+      data.current = true;
+      this.model = data.type;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
 
-<style></style>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    color: #ffff;
+    line-height: 36px;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius: 2px;
+  }
+  .current {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+}
+.login-form {
+  margin-top: 29px;
+  label {
+    display: block;
+    margin-bottom: 3px;
+    font-size: 14px;
+    color: #ffff;
+  }
+  .item-form {
+    margin-bottom: 13px;
+  }
+  .block {
+    display: block;
+    width: 100%;
+  }
+  .login-btn {
+    margin-top: 19px;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
   // 'dist', 生产环境构建文件的目录
   // assetsDir: 'static',
   // 相对于outputDir的静态资源(js、css、img、fonts)目录
-  lintOnSave: true,
+  lintOnSave: false,
   // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码
   // runtimeCompiler: true,
   // 是否使用包含运行时编译器的 Vue 构建版本
@@ -28,8 +28,14 @@ module.exports = {
     //     args[0].chunksSortMode = "none";
     //     return args;
     // });
-    // config.resolve.alias // 添加别名
-    // .set('@', resolve('src')).set('@assets', resolve('src/assets')).set('@components', resolve('src/components')).set('@views', resolve('src/views')).set('@store', resolve('src/store'));
+    config.resolve.extensions
+      .add('.js').add('.json').add('.vue');
+    config.resolve.alias // 添加别名
+      .set('@', '/src')
+      .set('@c', '/src/components')
+      .set('@views', '/src/views')
+      .set('@store', '/src/store')
+      .set('@utils', '/src/utils');
     // // 压缩图片
     // // 需要 npm i -D image-webpack-loader
     // config.module.rule("images").use("image-webpack-loader").loader("image-webpack-loader").options({
@@ -86,9 +92,9 @@ module.exports = {
       //             primary: '#333'
       //         }
       //     }
-      scss: {
-        prependData: `@import "~@/styles/main.scss";`
-      }
+      // scss: {
+      //   prependData: `@import "~@/styles/main.scss";`
+      // }
     }
   },
   devServer: {
